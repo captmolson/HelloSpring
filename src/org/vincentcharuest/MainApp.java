7fc45d9024180c3be05b9d8a46d677621cb36553
BuildTools <unconfigured@null.spigotmc.org>
2018-09-13 20:06:00 -0400

    initial commit

---
+++ src/org/vincentcharuest/MainApp.java
@@ -0,0 +1,18 @@
+package org.vincentcharuest;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args){
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+
+        objA.setMessage("I am object A");
+        objA.getMessage();
+
+        HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
+        objB.getMessage();
+
+    }
+}
